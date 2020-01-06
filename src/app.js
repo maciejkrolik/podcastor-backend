0115a56c8c1b2adfe8fb28186d283b30fe6d8947
Maciej Królik <maciejkrolik@outlook.com>
2020-01-06 15:01:46 +0100

    Add CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('./db/db');
 
@@ -6,6 +7,7 @@ const port = process.env.PORT;
 
 const userRouter = require('./routers/users');
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
