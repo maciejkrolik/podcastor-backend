daae6a204e82e64b23f4139d993b57ca96d15618
Maciej Królik <maciejkrolik@outlook.com>
2020-01-21 23:08:20 +0100

    Add new error messages

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,12 +2,19 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const auth = async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
+    let token = req.header('Authorization');
+
+    if (token) {
+        token = token.replace('Bearer ', '');
+    } else {
+        throw new Error('Authorization token not found.');
+    }
+
     try {
         const data = jwt.verify(token, process.env.JWT_KEY);
         const user = await User.findOne({_id: data._id, 'tokens.token': token});
         if (!user) {
-            throw new Error()
+            throw new Error('User not found.')
         }
         req.user = user;
         req.token = token;
